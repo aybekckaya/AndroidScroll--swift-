2f98d677ff44dfa250f19d81ea4e9d310619178d
aybek can kaya <aybekcankaya@aybeks-MacBook-Pro.local>
2015-09-13 20:19:51 +0300

    Initial Commit

---
+++ AndroidScroll/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AndroidScroll
+//
+//  Created by aybek can kaya on 13/09/15.
+//  Copyright (c) 2015 aybek can kaya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
