7ca35a7d64dbbfb8b71ebb5609cc22d4c58fa0bd
aybek can kaya <aybekcankaya@aybeks-MacBook-Pro.local>
2015-09-13 20:26:06 +0300

    init

---
+++ AndroidScroll/AndroidScroll.swift
@@ -0,0 +1,24 @@
+//
+//  AndroidScroll.swift
+//  AndroidScroll
+//
+//  Created by aybek can kaya on 13/09/15.
+//  Copyright (c) 2015 aybek can kaya. All rights reserved.
+//
+
+import UIKit
+
+class AndroidScroll: UIScrollView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+    
+    
+    
+
+}
---
+++ AndroidScroll/AndroidScrollCell.swift
@@ -0,0 +1,25 @@
+//
+//  AndroidScrollCell.swift
+//  AndroidScroll
+//
+//  Created by aybek can kaya on 13/09/15.
+//  Copyright (c) 2015 aybek can kaya. All rights reserved.
+//
+
+import UIKit
+
+class AndroidScrollCell: UIView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+    
+    
+    
+    
+
+}
